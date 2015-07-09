d369d167f58cb103fd682b223a5144903f447cd3
UshioShugo <ushio.s@gmail.com>
2015-07-09 17:33:50 +0900

    change name (no test)

--- Vagrantfile
+++ Vagrantfile
@@ -18,11 +18,19 @@ require './settings/current.rb'
 # import vm settings
 #
 Dir::glob("./servers/*.rb").each do |f|
-	require "#{f}"
+    require "#{f}"
 end
 
 Vagrant.configure("2") do |config|
 
-	# template server
-	config.vm.define(:api01, &$api01)
+
+    # 仮想環境にインストールするchefのバージョン
+    config.omnibus.chef_version = :latest
+
+    # berkshelfを有効にする
+    config.berkshelf.enabled = true
+    config.berkshelf.berksfile_path = "./Berksfile"
+
+    # template server
+    config.vm.define(:api01, &$api01)
 end
--- servers/api01.rb
+++ servers/api01.rb
@@ -1,19 +1,19 @@
-server = Proc.new{|conf|
-	conf.vm.box = 'ubuntu14_10'
-	conf.vm.hostname = "api01"
-	conf.vm.box_url = 'https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box'
+server = Proc.new{|config|
+	config.vm.box = 'ubuntu14_10'
+	config.vm.hostname = "api01"
+	config.vm.box_url = 'https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box'
 
-	conf.vm.network :private_network, ip: "192.168.100.10"
-	conf.vm.network :forwarded_port, guest:80, host:49152
+	config.vm.network :private_network, ip: "192.168.100.10"
+	config.vm.network :forwarded_port, guest:80, host:49152
 	
 	# share folder
-	conf.vm.synced_folder(
+	config.vm.synced_folder(
 		$share_folders[:api01],
 		"/mnt/api01",
 		:owner => 'vagrant', :group => 'vagrant'
 	) 
 	
-	conf.vm.provision :chef_solo do |chef|
+	config.vm.provision :chef_solo do |chef|
 		chef.cookbooks_path = $cookbooks_path
 		chef.roles_path = $roles_path
 		
@@ -23,17 +23,9 @@ server = Proc.new{|conf|
 		chef.json = {}
 	end
 
-	conf.vm.provider "virtualbox" do |vb|
+	config.vm.provider "virtualbox" do |vb|
 		vb.customize ["modifyvm", :id, "--memory", "512"]
 	end
-
-
-	# 仮想環境にインストールするchefのバージョン
-	conf.omnibus.chef_version = :latest
-
-	# berkshelfを有効にする
-	conf.berkshelf.enabled = true
-	conf.berkshelf.berksfile_path = "./Berksfile"
 }
 
 # Do not delete here!!
